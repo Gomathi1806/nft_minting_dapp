b7e624cc6c2bee807f541fe3bb5fede7ca5cf7c6
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2022-11-02 19:51:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg'
 import './App.css'
-import { MoralisProvider } from 'react-moralis'
+import algorand sdk
+
 import NftMinter from './pages/NFTMinter/NftMinter'
 
 function App () {
   const moralisServerUrl = process.env.REACT_APP_MORALIS_SERVER_URL
   const moralisAppId = process.env.REACT_APP_MORALIS_APP_ID
   return (
-    <MoralisProvider
+    
       appId='8fyJUFQbQLY1d2VZcErT3QNYis8eBRI8OILFk7bI'
       serverUrl='https://lrib7uhxuwbs.usemoralis.com:2053/server'
     >
@@ -17,7 +18,7 @@ function App () {
           <div className='App-overlay'></div>
         </main>
       </div>
-    </MoralisProvider>
+    
   )
 }
 
